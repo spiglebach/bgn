f913b6d9c9c83be810415f2018ed55b233d078a7
Szukács Marcell <spiglebach@gmail.com>
2024-04-21 23:31:46 +0200

    nextui configuration

---
+++ src/app/providers.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from "react";
+import {NextUIProvider} from "@nextui-org/react";
+
+interface ProvidersProps {
+  children: React.ReactNode
+}
+
+export default function Providers({children}: ProvidersProps) {
+  return (
+    <NextUIProvider>{children}</NextUIProvider>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,10 @@
 import type { Config } from 'tailwindcss'
+import {nextui} from '@nextui-org/react'
 
 const config: Config = {
   content: [
     './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
@@ -13,6 +15,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 }
 export default config
