40ba9901c3984d82854b599a76f8febff97b49d5
Szukács Marcell <spiglebach@gmail.com>
2024-04-25 21:37:32 +0200

    sortable card list

---
+++ src/components/game/SortableGameList.tsx
@@ -0,0 +1,81 @@
+'use client'
+
+import {Game} from "@/model/model";
+import {ReactNode, useState} from "react";
+import {Radio, RadioGroup} from "@nextui-org/react";
+
+export interface GameSortOption {
+  value: 'name' | 'complexity'
+  label: string
+}
+
+export interface OrderOption {
+  value: 'asc' | 'desc'
+  label: string
+}
+
+export interface GameWithCard {
+  game: Game
+  card: ReactNode
+}
+
+interface SortableGameListProps {
+  sortOptions: GameSortOption[]
+  gamesWithCard: GameWithCard[]
+  orderOptions: OrderOption[]
+  labels: {
+    sortBy: string
+    sortDirection: string
+  }
+}
+
+export const SortableGameList = ({sortOptions, gamesWithCard, orderOptions, labels: {sortBy, sortDirection}}: SortableGameListProps) => {
+  const [sort, setSort] = useState<'name'|'complexity'>('name')
+  const [order, setOrder] = useState<'asc'|'desc'>('asc')
+
+  let sortFunction = compareByName
+  if ('complexity' === sort) {
+    sortFunction = compareByComplexity
+  }
+
+  let orderMultiplier = 1
+  if ('desc' === order) {
+    orderMultiplier = -1
+  }
+
+  return (
+    <div>
+      <div className="flex gap-10 justify-center mb-10 border-1 border-black rounded-xl w-[30em] mx-auto bg-gray-100" >
+        <RadioGroup
+          className="p-3"
+          label={sortBy}
+          value={sort}
+          onChange={element => setSort(element.target.value)}
+        >
+          {sortOptions.map(option => <Radio key={option.value} value={option.value}>{option.label}</Radio>)}
+        </RadioGroup>
+        <RadioGroup
+          className="p-3"
+          label={sortDirection}
+          value={order}
+          onChange={element => setOrder(element.target.value)}
+        >
+          {orderOptions.map(option => <Radio key={option.value} value={option.value}>{option.label}</Radio>)}
+        </RadioGroup>
+      </div>
+      <div className="mx-auto flex flex-wrap justify-center gap-4 max-w-[80%]">
+        {gamesWithCard.sort((a, b) => (orderMultiplier * (sortFunction(a, b)))).map(gameWithCard => gameWithCard.card)}
+      </div>
+    </div>
+  )
+}
+
+function compareByComplexity(a: GameWithCard, b: GameWithCard) {
+  if (a.game.complexity < b.game.complexity) return -1
+  if (a.game.complexity > b.game.complexity) return 1
+  return 0
+}
+
+function compareByName(a: GameWithCard, b: GameWithCard) {
+  return a.game.name.localeCompare(b.game.name)
+}
--- src/components/game/game-list.tsx
+++ src/components/game/game-list.tsx
@@ -1,15 +1,30 @@
 import {games} from "@/data";
 import {GameCard} from "@/components/game/game-card";
 import {useTranslations} from "next-intl";
+import {GameWithCard, OrderOption, SortableGameList} from "@/components/game/SortableGameList";
+
+
 
 export const GameList = () => {
   const t = useTranslations("GameList")
 
+  const sortOptions: SortOption[] = [
+    {value: "name", label: t("Sorting.name")},
+    {value: "complexity", label: t("Sorting.complexity")}
+  ]
+
+  const orderOptions: OrderOption[] = [
+    {value: "asc", label: t("Sorting.Order.asc")},
+    {value: "desc", label: t("Sorting.Order.desc")}
+  ]
+
+  const gamesWithCard: GameWithCard[] = games.map(game => ({game: game, card : (<GameCard game={game} />)}))
+
   return (
     <div>
       <h2 className="text-2xl text-center mb-4">{t("header")}</h2>
-      <div className="mx-auto flex flex-wrap justify-center gap-4 max-w-[80%]">
-        {games.map(game => <GameCard game={game}/>)}
+      <div>
+        <SortableGameList sortOptions={sortOptions} gamesWithCard={gamesWithCard} orderOptions={orderOptions} labels={{sortBy: t("Sorting.sortBy"), sortDirection: t("Sorting.sortDirection")}} />
       </div>
     </div>
   )
