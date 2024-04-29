f29be91b4d7bd7e703083b912b834b3f0bf04715
Szukács Marcell <spiglebach@gmail.com>
2024-04-29 06:38:05 +0200

    remove main page padding

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-h-screen flex-col items-center">
       <div className="my-10">
         <NextOccasion/>
       </div>
