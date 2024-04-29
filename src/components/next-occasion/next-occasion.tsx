2dea462cc9fa5279a4b052c89c6071f3ae0c18bb
Szukács Marcell <spiglebach@gmail.com>
2024-04-29 06:37:11 +0200

    add no next occasion case

--- src/components/next-occasion/next-occasion.tsx
+++ src/components/next-occasion/next-occasion.tsx
@@ -4,6 +4,7 @@ import {gameOccasions} from "@/data";
 import {DateDisplay} from "@/components/next-occasion/date-display";
 import {useTranslations} from "next-intl";
 import {GameSuggestions} from "@/components/next-occasion/game-suggestions";
+import {Button} from "@nextui-org/react";
 
 export const NextOccasion = () => {
   const t = useTranslations("NextOccasion")
@@ -17,9 +18,15 @@ export const NextOccasion = () => {
     hours: t("CountdownTimer.NumberPanel.hours"),
     minutes: t("CountdownTimer.NumberPanel.minutes")
   }
-  return (
 
-    <div>
+  if (!nextGameOccasion) {
+    return (
+      <NoNextOccasion/>
+    )
+  }
+
+  return (
+    <div className="border-1 rounded-xl border-gray-400 px-10 pt-8 pb-14">
       <div>
         <Header level={1}>{t("CountdownTimer.header")}</Header>
         <Countdown targetTime={nextGameOccasionStartTime} numberPanelLabels={numberPanelLabels}/>
@@ -32,6 +39,18 @@ export const NextOccasion = () => {
       <div className="mt-10">
         <GameSuggestions/>
       </div>
+    </div>
+  )
+}
+
+const NoNextOccasion = () => {
+  const t = useTranslations("NextOccasion.Empty")
+  return (
+    <div className="border-1 rounded-xl border-gray-400 px-10 pt-8 pb-14">
+      <Header level={1} className="mb-7">{t("header")}</Header>
+      <div className="flex justify-center"><Button color="success">
+        {t("newButton")}
+      </Button></div>
     </div>
   )
 }
